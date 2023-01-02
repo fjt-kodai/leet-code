cc87302c4dc3098644fe6afcf9f932e47a4d5763
Kodai Fujita <fjt.kodai@gmail.com>
2023-01-02 23:52:34 +0900

    fix: add example

--- two_sum.go
+++ two_sum.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nums := []int{2, 7, 11, 15}
-	target := 9
+	target := 18
 
 	output := twoSum(nums, target)
 	fmt.Println(output)
@@ -15,11 +15,14 @@ func twoSum(nums []int, target int) []int {
 
 	for i, num := range nums {
 		index, ok := m[target-num]
+		// null, false := m[16]
+		// 2, true := m[11]
 		if ok {
 			return []int{index, i}
 		}
 
 		m[num] = i
+		// m[2] = 0
 	}
 
 	return []int{}
